19c0c73d7d60739c85201aa592c62fe7efc3a854
Sponton1x <kubawrzeszcz0@gmail.com>
2022-08-03 13:40:32 +0200

    add /src folder

--- engine.py
+++
@@ -1,7 +0,0 @@
-import pyttsx3
-
-def Engine():
-    engine = pyttsx3.init("sapi5")
-    voices = engine.getProperty('voices')
-    voices = engine.setProperty('rete', 100)
-    engine.setProperty('voice', "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0")
